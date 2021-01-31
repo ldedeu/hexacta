3884bcf96f5a28031bf3bca3c259df4ea36835f9
ldedeu <ldedeuaguilera@gmail.com>
2021-01-31 20:55:57 -0300

    Otra prueba.

--- src/com/br/teste/hexacta/Main.java
+++ src/com/br/teste/hexacta/Main.java
@@ -6,12 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        //int[] A = {-5, 7, 0, 3, -2, 12, 1, 1};
-        int[] A = {-5, -2, -15, -18, -25};
-        //int[] A = {9, 16, 8, 98, 105, 999};
-        //int[] A = {5, 8, 4, 10, 1, 2, 15, -18, 0, 3, 6, 7, 9, 11, 12, 13, 14};
+        //int[] A = {-5, 7, 0, 3, -2, 12, 1, 1}; //devolver 2
+        //int[] A = {-5, -2, -15, -18, -25}; //devolver 1
+        //int[] A = {9, 16, 8, 98, 105, 999}; //devolver 1
+        //int[] A = {5, 8, 4, 10, 1, 2, 15, -18, 0, 3, 6, 7, 9, 11, 12, 13, 14}; //devolver -1 porque esta toda la secuencia no hay menor que mostrar
+        int[] A = {998, -58, 0, 5, 7, 4, 1, 3, 2}; //devolver 6
         if (A.length > 100) {
-            System.out.println("El tamaño del arreglo no puede ser mayor a 100");
+            System.out.println("La longitud del arreglo no puede ser mayor a 100");
             return;
         }
         Solution solution = new Solution();
